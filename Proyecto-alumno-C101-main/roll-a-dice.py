04ee921d263729b4677e42412fe8df967268cc89
Felizvirusja <88406284+Felizvirusja@users.noreply.github.com>
2022-08-02 11:25:28 -0600

    Update roll-a-dice.py

--- Proyecto-alumno-C101-main/roll-a-dice.py
+++ Proyecto-alumno-C101-main/roll-a-dice.py
@@ -1,16 +1,11 @@
 import random 
   
-# Toma una variable para almacenar la respuesta y seguir tirando el dado
 response = "y"
    
 while response == "y": 
       
-    # Gnenera un número al azar 
-    # entre 1 y 6 (incluyendo 
-    # 1 y 6) 
     no = random.randint(1,6) 
 
-    # condiciones para verificar el valor del número  
     if no == 1: 
         print("[-----]") 
         print("[     ]") 
@@ -47,7 +42,6 @@ while response == "y":
         print("[     ]") 
         print("[0 0 0]") 
         print("[-----]") 
-          
-    # Pídele al usuario que ingrese una respuesta      
+             
     response=input("presiona y para girar otra vez y n para salir:") 
     print("\n") 
